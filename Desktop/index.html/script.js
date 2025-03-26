04b2681b6dc746b5ecf064ea01f6ab1960c8e02e
Autumn <jautumn810@gmail.com>
2025-03-25 23:03:43 -0500

    Initial commit: Add index.html, CSS, and JavaScript files

---
+++ Desktop/index.html/script.js
@@ -0,0 +1,11 @@
+
+function openMenu() {
+    document.getElementById("nav-links").style.display = "flex"; 
+    document.getElementById("hamburger-menu").style.display = "none"; 
+}
+
+
+function closeMenu() {
+    document.getElementById("nav-links").style.display = "none"; 
+    document.getElementById("hamburger-menu").style.display = "block"
+}
